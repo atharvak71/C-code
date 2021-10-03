6d910354d921704c34759d2c0707e5d699903acb
atharvak71 <69001125+atharvak71@users.noreply.github.com>
2021-10-03 12:35:43 +0530

    Create lsq_chebyshev_fit.h

---
+++ code/include/lsq_chebyshev_fit.h
@@ -0,0 +1,15 @@
+
+
+#ifndef __LSQ__
+#define __LSQ__
+
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+#include <string.h>
+#include <stdlib.h>
+#include "const.h"
+
+void lsq_chebyshev_fit(double s, int N, int M, double* T, double* A);
+
+#endif
