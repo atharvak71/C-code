9e91699cf337c7e67b0d374c60ccfc491781d1d9
atharvak71 <69001125+atharvak71@users.noreply.github.com>
2021-10-03 12:33:49 +0530

    Create ecef2eci.h

---
+++ code/include/ecef2eci.h
@@ -0,0 +1,15 @@
+
+
+#ifndef __ECEF__
+#define __ECEF__
+
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+#include <string.h>
+#include <stdlib.h>
+#include "const.h"
+
+void ecef2eci (double t,double* aB, double* acc);
+
+#endif
