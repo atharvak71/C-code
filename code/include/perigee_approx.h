31168627a609a1df077272e3802103703f566c08
atharvak71 <69001125+atharvak71@users.noreply.github.com>
2021-10-03 12:38:11 +0530

    Create perigee_approx.h

---
+++ code/include/perigee_approx.h
@@ -0,0 +1,15 @@
+
+
+#ifndef __PA__
+#define __PA__
+
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+#include <string.h>
+#include <stdlib.h>
+#include "const.h"
+
+void perigee_approx( double* X, double* normX, double* times, int n, double* rp, double* vp, double* tp, int* ind );
+
+#endif
