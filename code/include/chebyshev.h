9ce8364fb2f5f05334cd138dc3c6b10df415bc06
atharvak71 <69001125+atharvak71@users.noreply.github.com>
2021-10-03 12:31:11 +0530

    Create chebyshev.h

---
+++ code/include/chebyshev.h
@@ -0,0 +1,15 @@
+
+
+#ifndef __CHE__
+#define __CHE__
+
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+#include <string.h>
+#include <stdlib.h>
+#include "const.h"
+
+void chebyshev(double s, int N, int M, int arg, double* T);
+
+#endif
