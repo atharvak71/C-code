e8fe89f4c892271a831b778bb574c9c0d7e88a72
atharvak71 <69001125+atharvak71@users.noreply.github.com>
2021-10-03 12:43:21 +0530

    Create radial_gravity.h

---
+++ code/include/radial_gravity.h
@@ -0,0 +1,15 @@
+
+
+#ifndef __RG__
+#define __RG__
+
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+#include <string.h>
+#include <stdlib.h>
+#include "const.h"
+
+void radial_gravity(double* X, double tol, double deg, double* grav);
+
+#endif
