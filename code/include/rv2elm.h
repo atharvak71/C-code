f6b65a88d2168da01db119b645271327285b5429
atharvak71 <69001125+atharvak71@users.noreply.github.com>
2021-10-03 12:44:35 +0530

    Create rv2elm.h

---
+++ code/include/rv2elm.h
@@ -0,0 +1,15 @@
+
+
+#ifndef __RVE__
+#define __RVE__
+
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+#include <string.h>
+#include <stdlib.h>
+#include "const.h"
+
+void rv2elm(double* r, double* v, double tol, double* elm);
+
+#endif
