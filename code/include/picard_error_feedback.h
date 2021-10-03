7c8ab44596878bf50517a3a7caf633f120b97d58
atharvak71 <69001125+atharvak71@users.noreply.github.com>
2021-10-03 12:40:03 +0530

    Create picard_error_feedback.h

---
+++ code/include/picard_error_feedback.h
@@ -0,0 +1,15 @@
+
+
+#ifndef __PEF__
+#define __PEF__
+
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+#include <string.h>
+#include <stdlib.h>
+#include "const.h"
+
+void picard_error_feedback(double* X, double* del_X, double* del_a);
+
+#endif
